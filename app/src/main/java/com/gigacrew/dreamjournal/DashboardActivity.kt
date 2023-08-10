43bf8e172db6dc8c3492daa5fa45d5f37e9ed893
Supreetkler26 <supreetkler@gmail.com>
2023-08-10 00:38:26 -0400

    DashboardActivity UI and controller class updated

--- app/src/main/java/com/gigacrew/dreamjournal/DashboardActivity.kt
+++ app/src/main/java/com/gigacrew/dreamjournal/DashboardActivity.kt
@@ -1,48 +1,83 @@
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.SearchView
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import android.widget.TextView
+import androidx.core.view.isVisible
 import com.gigacrew.dreamjournal.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 
 class DashboardActivity : AppCompatActivity() {
 
-    private lateinit var textView2: TextView
+    private lateinit var menuButton: ImageButton
+    private lateinit var welcomeText: TextView
+    private lateinit var profileButton: ImageButton
+    private lateinit var filterButton: ImageButton
     private lateinit var searchView: SearchView
-    private lateinit var view: View
-    private lateinit var scrollView: RecyclerView
-    private lateinit var imageButton: ImageButton
-    private lateinit var button: Button
-    private lateinit var button5: Button
-    private lateinit var button6: Button
-    private lateinit var imageButton2: ImageButton
-    private lateinit var imageButton3: ImageButton
+    private lateinit var viewAllButton: Button
+    private lateinit var addButton: Button
+    private lateinit var recyclerView: RecyclerView
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
-        // Connect views to their corresponding XML elements using findViewById
-        textView2 = findViewById(R.id.textView2)
+        // Initialize your views
+        menuButton = findViewById(R.id.menuButton)
+        welcomeText = findViewById(R.id.welcomeText)
+        profileButton = findViewById(R.id.profileButton)
+        filterButton = findViewById(R.id.filterButton)
         searchView = findViewById(R.id.searchView)
-        view = findViewById(R.id.view)
-        scrollView = findViewById(androidx.appcompat.R.id.scrollView)
-        imageButton = findViewById(R.id.imageButton)
-        button = findViewById(R.id.button)
-        button5 = findViewById(R.id.button5)
-        button6 = findViewById(R.id.button6)
-        imageButton2 = findViewById(R.id.imageButton2)
-        imageButton3 = findViewById(R.id.imageButton3)
+        viewAllButton = findViewById(R.id.viewAllButton)
+        addButton = findViewById(R.id.addButton)
+        recyclerView = findViewById(R.id.recyclerView)
 
-        button.setOnClickListener {
-            // Your code for handling the button click goes here
-        }
+        // Set up RecyclerView
+        val layoutManager: LayoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
 
-        // ...
+
+        menuButton.setOnClickListener { /* Handle menu button click */ }
+
+
+        profileButton.setOnClickListener { /* Handle profile button click */ }
+
+
+        filterButton.setOnClickListener { /* Handle filter button click */ }
+
+
+        viewAllButton.setOnClickListener { /* Handle view all button click */ }
+
+
+        addButton.setOnClickListener {/* Handle add button click */ }
+
+
+
+        // Set a query listener for the search view
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // Handle search query submission
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                // Handle search query change
+                return true
+            }
+        })
+
+        val adapter = RecyclerViewAdapter() // Replace with your adapter class
+        recyclerView.adapter = adapter
     }
+}
+
+class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+
+
 }
